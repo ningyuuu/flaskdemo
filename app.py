5b6c399c7e10829cca689ee4e4239cb66e89a8c8
Ning Yu <ningyu321@gmail.com>
2018-06-29 11:41:11 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request, send_file
+import numpy as np
+import tensorflow as tf
+import model 
+import io
+from PIL import Image
+from shutil import copyfileobj
+from tempfile import NamedTemporaryFile
+
+app = Flask(__name__)
+sess = tf.Session()
+graph = model.load(sess)
+
+print('App init')
+
+@app.route('/tf', methods=['GET', 'POST'])
+def tf():
+  if request.method == 'GET':
+    return 'Please POST this endpoint with your 28x28 MNIST png as file.'
+
+  if 'file' not in request.files:
+    return jsonify({
+      'Error': '"file" in post request not found'
+    })
+
+  classify_file = request.files['file']
+
+  if classify_file.filename.rsplit('.', 1) [1] != 'png':
+    return jsonify({
+      'Error': '"file" not in .png format'
+    })
+
+  img = Image.open(io.BytesIO(classify_file.read())).convert('L')
+  img = img.resize((28, 28))
+  img = np.array(img)
+  img = np.expand_dims(img, axis=2)
+  img = np.expand_dims(img, axis=0)
+  response = model.predict(sess, graph, img)
+
+  return jsonify(response.tolist()[0])
+
+  # img_io = io.BytesIO()
+  # img.save(img_io, 'PNG', quality=90)
+  # img_io.seek(0)
+  # return send_file(img_io, mimetype='image/png')
---
+++ model.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import math
+import os
+from tensorflow.examples.tutorials.mnist import input_data as mnist_data
+
+mnist = mnist_data.read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+def build_graph():
+  x = tf.placeholder(tf.float32, [None, 28, 28, 1], name='x')
+  y_ = tf.placeholder(tf.float32, [None, 10], name='y_')
+  W = tf.Variable(tf.zeros([784, 10]))
+  b = tf.Variable(tf.zeros([10]))
+  xx = tf.reshape(x, [-1, 784])
+  y = tf.nn.softmax(tf.matmul(xx, W) + b)
+
+  cross_entropy = -tf.reduce_mean(y_ * tf.log(y)) * 1000.0 
+  train_step = tf.train.GradientDescentOptimizer(0.005, name='GradientDescent').minimize(cross_entropy)
+  classification = tf.argmax(y, 1, name='classification')
+
+  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+
+  return tf.get_default_graph()
+
+def build_better_graph():
+  pass
+
+def train(sess, graph, iters=10000):
+  x = graph.get_tensor_by_name('x:0')
+  y_ = graph.get_tensor_by_name('y_:0')
+  train_step = graph.get_operation_by_name('GradientDescent')
+  sess.run(tf.global_variables_initializer())
+
+  for r in range(iters):
+    if (r % 200) == 0:
+      print('Train:', r)
+    batch_X, batch_Y = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_X, y_: batch_Y})
+
+def predict(sess, graph, image):
+  x = graph.get_tensor_by_name('x:0')
+  classification = graph.get_tensor_by_name('classification:0')
+  return sess.run(classification, feed_dict={x: image})
+
+def save(sess, path_name='model', model_name='main'):
+  os.makedirs(os.path.join(os.path.dirname(os.path.realpath(__file__)), path_name, model_name), exist_ok=True)
+  saver = tf.train.Saver()
+  saver.save(sess, os.path.join(os.path.dirname(os.path.realpath(__file__)), path_name, model_name, 'model'))
+
+def load(sess, path_name='model', model_name='main'):
+  saver = tf.train.import_meta_graph(os.path.join(os.path.dirname(os.path.realpath(__file__)), path_name, model_name, 'model.meta'))
+  saver.restore(sess, tf.train.latest_checkpoint(os.path.join(os.path.dirname(os.path.realpath(__file__)), path_name, model_name)))
+  return tf.get_default_graph()
+
+def test(sess, graph):
+  x = graph.get_tensor_by_name('x:0')
+  y_ = graph.get_tensor_by_name('y_:0')
+  accuracy = graph.get_tensor_by_name('accuracy:0')
+  return sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+
+if __name__ == '__main__':
+  sess = tf.Session()
+  graph = load(sess)
+  train(sess, graph)
+  print('test', test(sess, graph))
+  save(sess)
